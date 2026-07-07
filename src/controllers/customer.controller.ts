600124c9c75d95e4699f8f2d1e82f69041eed151
Alex Bessa <alexbessa.dev@gmail.com>
2026-07-07 16:24:23 -0300

    refactor: utilizar definicao de tipo para dados de atualizacao de clientes

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
 import * as CustomerService from '../services/customer.service.ts';
-import type { CreateCustomer } from '../types.ts';
+import type { CreateCustomer, UpdateCustomer } from '../types.ts';
 
 export function getAllCustomers(_request: Request, response: Response): void {
 	const customers = CustomerService.findAllCustomers();
@@ -29,11 +29,7 @@ export function createCustomer(request: Request, response: Response): void {
 
 export function updateCustomer(request: Request, response: Response): void {
 	const id = Number(request.params.id);
-	const { name, email, status } = request.body as {
-		name: string;
-		email: string;
-		status: boolean;
-	};
+	const { name, email, status } = request.body as UpdateCustomer;
 
 	const customer = CustomerService.modifyCustomer(id, {
 		name,

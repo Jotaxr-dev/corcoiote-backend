782bfe488dc25e2454a9ace680ebab1f3a5da4a9
Alex Bessa <alexbessa.dev@gmail.com>
2026-07-15 12:16:44 -0300

    feat: conectar intermediarios para registro de requisicoes e manipulacao de erros

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler.ts';
+import requestLogger from './middlewares/requestLogger.ts';
 import CustomerRouter from './routes/customer.router.ts';
 
 const app = express();
 
+app.use(requestLogger);
 app.use(express.json());
 
 app.use('/customers', CustomerRouter);
@@ -12,5 +15,7 @@ app.use((_request, response) => {
 		message: 'Not found!'
 	});
 });
+
+app.use(errorHandler);
 
 app.listen(Number(process.env.PORT));

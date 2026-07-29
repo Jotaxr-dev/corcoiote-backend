d4fc09ef03dbc85eb00a1a47ba475d20cc972524
Alex Bessa <alexbessa.dev@gmail.com>
2026-07-29 16:32:24 -0300

    chore: difinir classe para erro de requisicao ruim

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,7 +1,19 @@
+import type { ValidationFieldError } from '../types.ts';
+
 export class NotFoundError extends Error {
 	statusCode = 404;
 
 	constructor(message: string) {
 		super(message);
+	}
+}
+
+export class BadRequestError extends Error {
+	statusCode = 400;
+	fields: ValidationFieldError[];
+
+	constructor(message: string, fields: ValidationFieldError[]) {
+		super(message);
+		this.fields = fields;
 	}
 }

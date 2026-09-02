963015b9f462ea581c1032876740de49b141dca2
Jota Xavier <jotax7.contato@gmail.com>
2026-09-02 00:34:22 +0000

    refactor: implementar mapeador objeto-relacional

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,9 @@
+import { deflate } from 'node:zlib';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../prisma/generated/client.ts';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-export const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
